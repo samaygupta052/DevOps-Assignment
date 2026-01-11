effa49506679ecd8618204455e7f59e4022855c5
samay <samaygupta052@gmail.com>
2026-01-11 07:44:23 +0000

    remove terraform files committed by mistake

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "backend_url" {
-  value = google_cloud_run_service.backend.status[0].url
-}
-
-output "frontend_url" {
-  value = google_cloud_run_service.frontend.status[0].url
-}
-
--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_id" {}
-variable "region" {
-  default = "us-central1"
-}
-
-variable "backend_image" {}
-variable "frontend_image" {}
-
